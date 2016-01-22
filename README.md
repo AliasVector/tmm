dab8c7c44ca8d3e55f8faa6bb8354be3f98614ac
Roman <aliasvector@gmail.com>
2016-01-22 15:21:03 +0600

    Initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tmm
 Theory mechanisms and machines
+
+This project should allow create mechanisms and perfporms their drawing.
---
+++ tmm-model/src/main/java/com/mar/tmm/model/Mechanism.java
@@ -0,0 +1,8 @@
+package com.mar.tmm.model;
+
+/**
+ * Interface to describe the common behavior for all kind of mechanisms.
+ */
+public interface Mechanism {
+    // ToDo
+}
