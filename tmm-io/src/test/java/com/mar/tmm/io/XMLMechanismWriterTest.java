89d6f3914fea2c80678c2953c9ce398fffea6809
Roman <aliasvector@gmail.com>
2016-02-16 11:50:30 +0600

    InputOutput  * continued tests

--- tmm-io/src/test/java/com/mar/tmm/io/XMLMechanismWriterTest.java
+++ tmm-io/src/test/java/com/mar/tmm/io/XMLMechanismWriterTest.java
@@ -1,7 +1,12 @@
 package com.mar.tmm.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.mar.tmm.io.impl.XMLMechanismWriter;
@@ -17,6 +22,7 @@ import org.junit.Test;
 public class XMLMechanismWriterTest {
 
     private static final String TEST_XML_FILE = "./test.xml";
+    private static final String REFERENCE_XML_RESOURCE = "/test.xml";
 
     private Mechanism createTestRotationMechanism() {
         final Mechanism testMechanism = MechanismUtils.createMechanismWithRotationKinematicPair("Test mechanism",
@@ -36,19 +42,41 @@ public class XMLMechanismWriterTest {
         return testMechanism;
     }
 
-    private void checkXMLFile(final String fileName) {
+    private void checkXMLFile(final String fileName) throws IOException {
         final File file = new File(fileName);
 
         assertTrue("File doesn't exist", file.exists());
+        final String referenceFile = readFile(getClass().getResource(REFERENCE_XML_RESOURCE).getPath());
+        final String testFile = readFile(fileName);
+
+        assertEquals("Result is not correct", referenceFile, testFile);
     }
 
+    private void removeXMLFile(final String fileName) throws IOException {
+        final File file = new File(fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    private String readFile(final String filename) throws IOException {
+        final List<String> lines = Files.readAllLines(Paths.get(filename));
+        final StringBuilder sb = new StringBuilder(1024);
+        for (final String line : lines) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+
     @Test
-    public void testMarshallingMechanism() {
+    public void testMarshallingMechanism() throws IOException {
         final Mechanism testMechanism = createTestRotationMechanism();
 
         final XMLMechanismWriter writer = XMLMechanismWriter.createInstance(TEST_XML_FILE);
         writer.writeMechanism(testMechanism, true);
 
         checkXMLFile(TEST_XML_FILE);
+        removeXMLFile(TEST_XML_FILE);
     }
 }
